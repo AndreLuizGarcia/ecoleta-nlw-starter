adfdec4871cb6c8ed5480069f56c676698f3c649
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-06-04 20:06:59 -0300

    scripts create point page relateds

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,12 +25,14 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
   
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
+
   fetch(url)
   .then( res => res.json() )
   .then( cities => {
-
     for(city of cities) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
 
     citySelect.disabled = false
@@ -40,3 +42,52 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+// Itens de coleta
+// pegar todos os li`s
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+
+  // Adicionar ou remover uma classe com JS
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id
+
+  // verificar se existem itens selecionados, se sim
+  // pegar os itens selecionados
+
+  const alreadySelected = selectedItems.findIndex( item => {
+    const itemFound = item === itemId
+
+    return itemFound
+  })
+
+  // se já estiver selecionado
+  if ( alreadySelected >= 0 ) {
+    // tirar a selecao
+
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDifferent = item != itemId
+
+      return itemIsDifferent
+    })
+
+    selectedItems = filteredItems
+  } else {
+    // se não estiver selecionado, adicionar à seleção
+    selectedItems.push(itemId)
+  }
+
+  // atualizar o campo escondido com os itens selecionados
+  collectedItems.value = selectedItems
+}
